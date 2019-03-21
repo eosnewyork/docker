3a9e49fd9d6048b413622edfd4297d550ee2c8f6
deckb <deck@eosnewyork.io>
2019-03-21 16:28:37 +0000

    update builds

--- eos/build_eos.sh
+++ eos/build_eos.sh
@@ -6,19 +6,27 @@ if [ "$#" -ne "2" ]; then
     exit 1
 fi
 
-ubuntu_version=${2}
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+builder_tag=${2}
 version=${1}
 docker_prefix=eosnewyork
 
 docker build \
-       --build-arg ubuntu_version=${ubuntu_version} \
+       --build-arg builder_tag=${builder_tag} \
        --build-arg version=v${version} \
        --tag ${docker_prefix}/eos:${version}_${ubuntu_version} \
-       .
+       ${cwd}
+
+if [ "${?}" -ne "0" ]; then
+    echo "Build failed!"
+    exit 1
+fi
+
 
 # run docker image and copy build files locally
 docker run \
-       -v `pwd`:/mnt/build \
+       -v ${cwd}:/mnt/build \
        -it ${docker_prefix}/eos:${version}_${ubuntu_version} \
        /bin/bash -c "ls /eos/build/packages && cp /eos/build/packages/eosio_${version}-1_amd64.deb /mnt/build/"
        #"ls /eos/build/package && cp /eos/build/package/eosio_${version}-1_amd64.deb /mnt/build/"
--- eos/eoschronicle/build_chronicle.sh
+++ eos/eoschronicle/build_chronicle.sh
@@ -5,13 +5,13 @@ if [ "$#" -ne "1" ]; then
     echo "example: ./build_contracts.sh 1.0"
     exit 1
 fi
-
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 version=${1}
 docker_prefix=eosnewyork
 
 docker build \
        --build-arg version=v${version} \
        --tag ${docker_prefix}/eos-chronicle:${version} \
-       .
+       ${cwd}
 
 # run docker image and copy build files locally
