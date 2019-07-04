c7c1f560268259fa88ca29dd2d9d110a061b1abd
Buddy Deck <deck@eosnewyork.io>
2019-07-04 13:54:51 +0000

    update contract to handle new locations

--- eos/contracts/build_contracts.sh
+++ eos/contracts/build_contracts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-if [ "$#" -ne "4" ]; then
+#/root/eosio/1.8/src/boost_1_70_0
+if [ "$#" -ne "5" ]; then
     echo "Need to pass cdt, eos, ubuntu, and contract versions"
-    echo "example: ./build_contracts.sh 1.5.0 1.6.2 18.04 1.5.2"
+    echo "example: ./build_contracts.sh 1.5.0 1.6.2 18.04 1.5.2 /root/eosio/1.8/src/boost_1_70_0"
     exit 1
 fi
 
@@ -10,12 +10,13 @@ ubuntu_version=${3}
 eos_version=${2}
 version=${4}
 cdt_version=${1}_${eos_version}_${ubuntu_version}
+boost_location=${5}
+echo boost_loc=${boost_location}
 docker_prefix=eosnewyork
 
 docker build \
        --build-arg cdt_version=eos.cdt:${cdt_version} \
        --build-arg version=v${version} \
+       --build-arg boost_location=${boost_location} \
        --tag ${docker_prefix}/eos.contracts:${version}_${cdt_version} \
        .
-
-# run docker image and copy build files locally
